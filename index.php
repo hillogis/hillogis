32efc8c16fdc50685f7ead91ec699811b0456640
Hillary Odongo <odongohillary63@gmail.com>
2024-10-22 14:43:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,428 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>SORI HIGH</title>
+<meta name="description" content="SORI HIGH SCHOOL">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css"href="master.css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrap/3.2.0/css/bootstrap.theme.mini.css">
+<nav class="navbar navbar-inverse navbar-fixed-top" id="my navbar">
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="navbar-collapse">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span> 
+</button> 
+  </div>
+<div class="collapse navbar-collapse" id="navbar-collapse">
+<a href=""class="btn btn-warning navbar-btn navbar-right">Download now</a>
+<ul class="nav navbar-nav">
+<li><a href="ABOUT US.HTML"class="navbar-brand">ABOUT US</a>
+<li><a href="contact.php"class="navbar-brand">CONTACT US</a>
+<li><a href="ACADEMICS.HTML"class="navbar-brand">ACADEMICS</a>
+<li><a href="login2.php" class="navbar-brand">STUDENTS</a>
+<li><a href="Downloads.html" class="navbar-brand navbar-right">DOWNLOADS</a>
+</ul>
+<ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+</div>
+</div>
+</nav>
+<div class ="jumbotron">
+<div class="container text-center">
+<div id="grad">
+<img src="pictures/LOGO.PNG" width="100" height="75" alt="text of the image"/>
+<b> WELCOME TO SORI BOYS HIGH SCHOOL</b>
+<img src="pictures/LOGO.PNG" width="100" height="75" alt="text of the image"/><br><br>
+<br>
+<P class="one"> THE CENTER OF EXELLENCE</P>
+<div class="dropdown">
+  <button class="dropbtn">ABOUT US</button>
+  <div class="dropdown-content">
+    <a href="ABOUT US.HTML">school history</a>
+	<a href="clubs and societies.php">clubs and societies</a>
+	<a href="loginnnn.php">Games & Sports</a>
+	<a href="students council.php">Students Council</a>
+	<a href="Administration Structure.php">Administration Structure</a>
+  </div>
+</div> 
+<div class="dropdown">
+  <button class="dropbtn"> CONTACT</button>
+  <div class="dropdown-content">
+    <a href="contact.php">Service Desk</a>
+	<a href="Adverts.php">School Adverts</a>
+	<a href="Sec.php">Principal Sec Desk</a>
+  </div>
+</div> 
+<div class="dropdown">
+  <button class="dropbtn">ACADEMICS</button>
+  <div class="dropdown-content">
+    <a href="ACADEMICS.html">Academic Departments</a>
+	 <a href="login4.php">Registra</a>
+	 	 <a href="login4.php">Library</a>
+		 	 <a href="class.php">Inter-Classes</a>
+  </div>
+</div> 
+<div class="dropdown">
+  <button class="dropbtn">STUDENTS</button>
+  <div class="dropdown-content">
+    <a href="login2.php">New Students</a>
+	 <a href="loginnnn.php">Catering</a>
+	 <a href =""> studen portal</a>
+	 <a href="login2.php">Alumni</a>
+	 <a href ="">students downloads</a>
+  </div>
+</div> 
+
+<div class="dropdown">
+  <button class="dropbtn">ADMINISTRATION</button>
+  <div class="dropdown-content">
+    <a href="loginnn.php">Finance</a>
+    <a href="login3.php">Sub-Bordinate staff</a>
+    <a href="loginnnn.php">Catering Department</a>
+	  <a href="loginnn.php">Major Events</a>
+	    <a href="loginnn.php">Rules and Regulations</a>
+	
+  </div>
+</div>
+</div>
+</div><!---end of grad--->
+</head>
+<hr>
+
+<BODY>
+<div class="container text-center">
+
+<section>
+<div class="carousel slide" id="screenshot-carousel" data-ride="carousel">
+<ol>
+<div class="carousel-caption">
+<li data-target="screenshot-carousel"data-slide-to"0"> </li>
+<li data-target="screenshot-carousel"data-slide-to"1"></li>
+<li data-target="screenshot-carousel"data-slide-to"2"></li>
+<li data-target="screenshot-carousel"data-slide-to"3"></li>
+</ol>
+<div class="carousel-inner">
+<div class="item active">
+<img src="pictures/t11.jpg" width="980" height="150" alt="text of the image"/>
+ <div class="carousel-caption">
+ <h3>CLASSROOMS</h3>
+ <P>This is the classroom</p>
+ </div>
+</div>
+<div class="item">
+<img src="pictures/t13.jpg" width="980" height="150" alt="text of the image"/>
+<div class="carousel-caption">
+ <h3>ADMIN BLOCK</H3>
+ <P>This is the Administration Block</p>
+ </div>
+</div>
+<div class="item">
+<img src="pictures/t14.jpg" width="980" height="150"alt="text of the image"/>
+<div class="carousel-caption">
+ <h3>LIBRARY</H3>
+ <P>This is our Library</p>
+ </div>
+</div>
+<div class="item">
+<img src="pictures/t15.jpg" width="980" height="150" alt="text of the image" />
+<div class="carousel-caption">
+ <h3>VISSION</H3>
+ <P>This is our Vission</p>
+ </div>
+</div>
+</div><!--end carousel inner-->
+</div><!--end carousel-->
+</section>
+</div>
+<marquee direction="right"bgcolor="pink">
+<img src= "pictures/t13.jpg"  height="50" width="125" border="4"/>
+<img src= "pictures/t14.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t15.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/ft.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/nv.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/sc.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t1.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t2.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t3.jpg"  height="50" width="125" border="4"/>
+<img src= "pictures/t4.jpg"  height="50" width="125" border="4"/>
+<img src= "pictures/t5.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t6.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t7.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t8.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t9.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t10.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t11.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t12.jpg"  height="50" width=c"125" border="4"/>
+</marquee>
+<hr>
+<center>
+<button type="button" class="btn btn-danger"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+<div class="btn-group">
+  <button type="button" class="btn btn-info">Honesty</button>
+  <button type="button" class="btn btn-info">Fairness</button>
+  <button type="button" class="btn btn-info">Integrity</button>
+  <button type="button" class="btn btn-info">Discipline</button>
+  <button type="button" class="btn btn-info">Transparency</button>
+  <button type="button" class="btn btn-info">Self-control</button>
+  <button type="button" class="btn btn-info">self-esteem</button>
+  <button type="button" class="btn btn-info">Trustworthiness</button>
+</div>
+</centre>
+<hr>
+<div class="container">
+<section>
+<div class="row">
+<div class="col-lg-4">
+<div class="panel panel-default">
+<div class="panel-body">
+<blockquote>
+<p> OUR VISSION</p>
+<p><img src="pictures/t14.jpg" class="img-thumbnail" height="100" width="200" border="4"/></p>
+<footer>To be a leading institution in the production of manpower that will be in high demand in the society.</footer>
+</blockquote>
+</div>
+</div>
+</div>
+
+<div class="col-lg-4">
+<div class="panel panel-default">
+<div class="panel-body">
+<blockquote>
+<p>OUR MISSION</p>
+<video width="200" class="img-thumbnail" height="100"controls>
+  <source src="pictures/tr.mp4" type="video/mp4">
+</video>
+<footer>To create an enabling environment to prepare learners for their roles in the society and the challenges that modern world presents.</footer>
+</blockquote>
+</div>
+</div>
+</div>
+<div class="col-lg-4">
+<div class="panel panel-default">
+<div class="panel-body">
+<blockquote>
+<p>OUR MOTTO</p>
+<p><img src="pictures/t3.jpg" class="img-thumbnail" height="140" width="200" border="4"/></p>
+<footer>Nothing But The Best<br>
+SLOGAN:<BR>
+"The Home Of The Giants, Heroes And Kings In The Todays World"</footer>
+</blockquote>
+</div>
+</div>
+</div>
+</div>
+</section>
+</div>
+<hr>
+<div class="row">
+<div class="col-lg-12">
+<div class="panel panel-default">
+<div class="panel-body">
+<header><centre>MISSION</header>
+<li> To create an enabling environment to prepare learners for their roles in the society and the challenges that modern world presents.</li>
+</div>
+</div>
+</div>
+</div>
+<hr>
+   <div class="row">
+<div class="col-lg-6">
+<div class="panel panel-default">
+<div class="panel-body">
+ <img src="pictures/nv.jpg" class="img-rounded" alt="Cinque Terre" width="320" height="350">
+	   <p class="text-primary">MR. OCHIENG' KENAS RAKIRO </p>
+</div>
+</div>
+</div>
+<div class="col-lg-6">
+<div class="panel panel-default">
+<div class="panel-body">
+<p class="one">PRINCIPAL MESSAGE</p>
+ <li>In this era of cut throat competition, 
+I express my heartfelt gratitude to Sori High School for providing me with this unique opportunity to giving shape to 'Royal Excellence' as yet another 'first initiative' among many other such initiatives accomplished during the last few months.
+I wish to put on records the incessant assistance and cooperation from all my teaching and non-teaching colleagues and students  as a whole.l again appreciate the surrounding community for providing a conducive environment that has enable the students to be free from noice pollution and as promoted learning capabilities of many of our students. Not to forget the Parents Teachers Association for their frantic effort of ensuring that we get the enough human resource that has help to build our students in various ares in their studies. May the Almighty Bless The Fraternity of the Sori High School as a Whole</li>
+</div>
+</div>
+</div>
+</div>
+<hr>
+<centre>
+<div class="btn-group">
+  <button type="button" class="btn btn-success">Honesty</button>
+  <button type="button" class="btn btn-success">Fairness</button>
+  <button type="button" class="btn btn-success">Integrity</button>
+  <button type="button" class="btn btn-success">Discipline</button>
+  <button type="button" class="btn btn-success">Transparency</button>
+  <button type="button" class="btn btn-success">Self-control</button>
+  <button type="button" class="btn btn-success">self-esteem</button>
+  <button type="button" class="btn btn-success">Trustworthiness</button>
+</div>
+</centre>
+<hr>
+<div id="content">
+<P><centre>THE BOARD OF MANAGEMENT</P>
+</div>
+<div class="row">
+<div class="col-lg-4">
+<div class="panel panel-default">
+<div class="panel-body">
+<OL TYPE="1">
+<oL TYPE="1" START ="1">
+<P>B.O.G MEMBERS</P>
+<LI>MR. OUGO </LI>
+<LI>MR. NYAWANGA  </LI>
+<LI>MR. PHILIP </LI>
+<li>FR. CLAUDIO </LI>
+<LI>MR. ABADE </LI>
+<LI>ENG. ABONYO </LI>
+</div>
+</div>
+</div>
+<div class="col-lg-4">
+<div class="panel panel-default">
+<div class="panel-body">
+<img src="pictures/T9.jpg"  height="130" width="200" boarder="2"/>
+<P>THE SCHOOL B.O.G </P>
+</div>
+</div>
+</div>
+<div class="col-lg-4">
+<div class="panel panel-default">
+<div class="panel-body">
+<OL TYPE="1">
+<oL TYPE="1" START ="7">
+<P>B.O.G MEMBERS</P>
+<LI>MRS. JAMWAKA </LI>
+<LI>MR. RAKIRO </LI>
+<LI>MR. SAGGIA </LI>
+<LI>MR. KASERA </LI>
+<LI>MR. MIKULA </LI>
+<LI>MRS. NGAJI</LI>
+</div>
+</div>
+</div>
+</div>
+<hr>
+<centre>
+ <div class="btn-group">
+  <button type="button" class="btn btn-success">Honesty</button>
+  <button type="button" class="btn btn-success">Fairness</button>
+  <button type="button" class="btn btn-success">Integrity</button>
+  <button type="button" class="btn btn-success">Discipline</button>
+  <button type="button" class="btn btn-success">Transparency</button>
+  <button type="button" class="btn btn-success">Self-control</button>
+  <button type="button" class="btn btn-success">self-esteem</button>
+  <button type="button" class="btn btn-success">Trustworthiness</button>
+</div>
+</centre>
+<hr>
+<div class="container">
+<section>
+<div class="page-header" id="Gallery">
+<h3>Gallery</h3>
+<div class="row">
+<div class="col-lg-4">
+<blockquote>
+<div id="img">
+<p><img src="pictures/t12.jpg" class="img-rounded" alt="Cinque Terre" height="100" width="200" border="4"/></p>
+<footer>SCHOOL LIBRARY</footer>
+</div>
+</blockquote>
+</div>
+<div class="col-lg-4">
+<blockquote>
+<div id="img">
+<p><img src="pictures/t13.jpg" class="img-rounded" alt="Cinque Terre"  height="100" width="200" border="4"/></p>
+<footer> ACADEMIC BLOCK</footer>
+</div>
+</blockquote>
+</div>
+<div class="col-lg-4">
+<blockquote>
+<div id="img">
+<p><img src="pictures/t14.jpg" class="img-rounded" alt="Cinque Terre"  height="100" width="200" border="4"/></p>
+<footer> LIBRARY BOOKSHELF</footer>
+</div>
+</blockquote>
+</div>
+<a href="screenshot-carousel" class="left carousel-control" data-slide="prev">
+<span class="glyphicon glyphicon-chevron-left"></span>
+</a>
+<a href="screenshot-carousel" class="right carousel-control" data-slide="next">
+<span class="glyphicon glyphicon-chevron-right"></span>
+</a>
+</div><!--end row-->
+<div class="row">
+<div class="col-lg-12">
+<div class="panel panel-default">
+<div class="panel-body">
+<hr>
+<header><centre>VISSION</header>
+<li>To be a leading institution in the production of manpower that will be in high demand in the society.</li>
+<hr>
+</div>
+</div>
+</div>
+</section>
+</div>
+<hr>
+<marquee direction="right"bgcolor="pink">
+<img src= "pictures/t13.jpg"  height="50" width="125" border="4"/>
+<img src= "pictures/t14.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t15.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/ft.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/nv.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/sc.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t1.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t2.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t3.jpg"  height="50" width="125" border="4"/>
+<img src= "pictures/t4.jpg"  height="50" width="125" border="4"/>
+<img src= "pictures/t5.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t6.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t7.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t8.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t9.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t10.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t11.jpg"  height="50" width="125" border="4"/>
+<img src="pictures/t12.jpg"  height="50" width="125" border="4"/>
+</marquee>
+<hr>
+
+        <b><centre><p>All Rights Reserved &copy; 2017 </p></ccentreenter></b>
+		<hr>
+		<centre>
+		<div class="btn-group">
+  <button type="button" class="btn btn-danger">Honesty</button>
+  <button type="button" class="btn btn-primary">Fairness</button>
+  <button type="button" class="btn btn-primary">Integrity</button>
+  <button type="button" class="btn btn-success">Discipline</button>
+  <button type="button" class="btn btn-success">Transparency</button>
+  <button type="button" class="btn btn-success">Self-control</button>
+  <button type="button" class="btn btn-info">self-esteem</button>
+  <button type="button" class="btn btn-info">Trustworthiness</button>
+  <button type="button" class="btn btn-danger">Honesty</button>
+</div>
+</div>
+</div>
+
+   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+   </body>
+   
+   </HTML>
+
+
+
